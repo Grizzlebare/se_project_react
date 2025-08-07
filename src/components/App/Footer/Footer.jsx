e29f01e4c2d99bace1f0f9a2f0b81970fcca8e1c
Kuma_Lord <eddison.torres@gmail.com>
2025-08-07 00:13:49 -0400

    Pre-API implementation

---
+++ src/components/App/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer__container">
+      <p className="developer">Developed by Eddison T.</p>
+      <p className="year">2025</p>
+    </footer>
+  );
+}
+
+export default Footer;
